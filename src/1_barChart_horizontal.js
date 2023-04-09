c669b0720248833f99eab0f31de078dbbf645947
guedelka <karin.guedel@fhnw.ch>
2023-04-09 14:13:59 +0200

    horizontal bar chart with real data

---
+++ src/1_barChart_horizontal.js
@@ -0,0 +1,86 @@
+/**** Chart 1: Bar Chart Life Quality vs. Threatened Species ****/
+/** Source: https://d3-graph-gallery.com/graph/barplot_button_data_csv.html **/
+
+/**** Chart 1: Bar Chart Life Quality vs. Threatened Species ****/
+/** Source: https://d3-graph-gallery.com/graph/barplot_button_data_csv.html **/
+
+// set the dimensions and margins of the graph
+const margin = {top: 30, right: 30, bottom: 70, left: 60},
+    width = 600 - margin.left - margin.right,
+    height = 700 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+const svg1b = d3.select("#my_barchart_horizontal")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+
+// Initialize the X axis
+const x = d3.scaleLinear()
+    .range([0, width])
+
+const xAxis = svg1b.append("g")
+    .attr("transform", `translate(0,${height})`);
+
+// Initialize the Y axis
+const y = d3.scaleBand()
+    .range([0, height])
+    .padding(0.2)
+const yAxis = svg1b.append("g")
+    .attr("class", "myYaxis");
+
+
+// A function that create / update the plot for a given variable:
+function update(selectedVar) {
+
+    // Parse the Data
+    d3.csv("./data/1_living_quality_stacked.csv").then(function (data) {
+        console.log(data)
+        console.log(selectedVar)
+
+        // Add Y axis
+        y.domain(data.map(d => d.country));
+        yAxis.transition().duration(1000)
+            .call(d3.axisLeft(y))
+            .style("text-anchor", "right");
+        //todo: align text at the y-axis
+
+        // X axis
+        x.domain([0, d3.max(data, d => d[selectedVar])])
+        console.log(d3.max(data, d => d[selectedVar]))
+
+        xAxis.transition().duration(1000)
+            .call(d3.axisBottom(x))
+            .selectAll("text")
+            .attr("transform", "translate(5,0)")
+            .style("text-anchor", "end");
+
+        // variable u: map data to existing bars
+        const u = svg1b.selectAll("rect")
+            .data(data)
+
+        // update bars
+        u.join("rect")
+            .transition()
+            .duration(1000)
+            .attr("x",d => x(0))
+            .attr("y", d => y(d.country))
+
+            .attr("width", d => x(d[selectedVar]))
+            .attr("height", y.bandwidth())
+            .attr("fill", "red")
+        //todo: different fill depending on selected var (red for threatened species; green for life quality)
+
+    })
+}
+
+function sort(selectedVar) {
+    //todo sort the selectedVar
+}
+
+// Initialize plot
+update('Total Index')
+
