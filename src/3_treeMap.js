7499e973a05159ead42d8c7bdce0b3ef0af737aa
Karin Güdel <karin.guedel@fhnw.ch>
2023-04-23 16:30:34 +0200

    Develop

--- src/3_treeMap.js
+++ src/3_treeMap.js
@@ -1,4 +1,5 @@
 /**** Chart 3: Treemap of Area of Biotopes and number of animals living there ****/
+
 /** Possible Source: https://d3-graph-gallery.com/graph/treemap_custom.html**/
 
     // set the dimensions and margins of the graph
@@ -8,20 +9,23 @@
 
 // append the svg object to the body of the page
 const svg3 = d3.select("#my_treemap")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-        `translate(${margin.left}, ${margin.top})`);
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            `translate(${margin.left}, ${margin.top})`);
 
 // read json data
-d3.json("./data/3_test_data.json").then(function (data) {
-    console.log(data)
+d3.json("./data/3_treemap_CH.json").then(function (data) {
+    //console.log(data)
+
     // Give the data to this cluster layout:
-    const root = d3.hierarchy(data).sum(function (d) {
-        return d.value
-    }) // Here the size of each leave is given in the 'value' field in input data
+    const root = d3.hierarchy(data)
+        .sum(function (d) {
+            return d.value
+        }) // Here the size of each leave is given in the 'value' field in input data
+        .sort((a, b) => a.height - b.height || a.value - b.value);
 
     // Then d3.treemap computes the position of each element of the hierarchy
     d3.treemap()
@@ -33,16 +37,17 @@ d3.json("./data/3_test_data.json").then(function (data) {
         //.padding(20)
         (root)
 
+
     // prepare a color scale
 
-    const color = d3.scaleOrdinal()
-        .domain(["Insekten","Reptilien", "Fische",  "Säugetiere", "Vögel", "Amphibien"])
-        .range(["#ffa8f2", "#dec2ff", "#70fffa", "#eaff70", "#ffc170", "#b5a8ff"])
+     const color = d3.scaleOrdinal()
+       //.domain(["Reptilien", "Fische",  "Säugetiere", "Vögel", "Amphibien"])
+    .range(["#ff230a", "#21caf1", "#ff4c38", "#ff6352", "#eaff70"])
 
     // And a opacity scale
-    const opacity = d3.scaleLinear()
-        .domain([500, 0])
-        .range([0, 1])
+    // const opacity = d3.scaleLinear()
+    //     .domain([100, 0])
+    //     .range([0, 1])
 
     // use this information to add rectangles:
     svg3
@@ -61,34 +66,35 @@ d3.json("./data/3_test_data.json").then(function (data) {
         .attr('height', function (d) {
             return d.y1 - d.y0;
         })
-        .style("stroke", "black")
-        //.style("fill", function (d) {
-        // return color(d.data.color)
-        // })
+        //.style("stroke", "red")
         .style("fill", function (d) {
-            return color(d.parent.data.name)
+            return color(d.data.name)
         })
-        .style("opacity", function (d) {
-            return opacity(d.data.value)
-        })
+    //    .style("fill", function(d) { return d.data.colorsamp; })
+    //  .style("fill", function (d) {
+    //      return color(d.parent.data.colorsamp)
+    //  })
+    // .style("opacity", function (d) {
+    //     return opacity(d.data.value)
+    // })
 
-    // and to add the text labels
-    // svg3
-    //     .selectAll("text")
-    //     .data(root.leaves())
-    //     .enter()
-    //     .append("text")
-    //     .attr("x", function (d) {
-    //         return d.x0 + 5
-    //     })    // +10 to adjust position (more right)
-    //     .attr("y", function (d) {
-    //         return d.y0 + 20
-    //     })    // +20 to adjust position (lower)
-    //     .text(function (d) {
-    //         return d.data.name.replace('mister_', '')
-    //     })
-    //     .attr("font-size", "12px")
-    //     .attr("fill", "white")
+    //and to add the text labels
+    svg3
+        .selectAll("text")
+        .data(root.leaves())
+        .enter()
+        // .append("text")
+        // .attr("x", function (d) {
+        //     return d.x0 + 5
+        // })    // +10 to adjust position (more right)
+        // .attr("y", function (d) {
+        //     return d.y0 + 20
+        // })    // +20 to adjust position (lower)
+        // .text(function (d) {
+        //     return d.data.name.replace('mister_', '')
+        // })
+       // .attr("font-size", "12px")
+        //.attr("fill", "white")
 
     // and to add the text labels
     svg3
@@ -97,47 +103,49 @@ d3.json("./data/3_test_data.json").then(function (data) {
         .enter()
         .append("text")
         .attr("x", function (d) {
-            return d.x0 +7
+            return d.x0 + 1
         })    // +10 to adjust position (more right)
         .attr("y", function (d) {
-            return d.y0 + 10
+            return d.y0 + 12
         })    // +20 to adjust position (lower)
         .text(function (d) {
             return d.data.value
         })
-        .attr("font-size", "30px")
+        .attr("font-size", "14px")
         .attr("fill", "black")
 
     // Add title for the 3 groups
     svg3
         .selectAll("titles")
         .data(root.descendants().filter(function (d) {
-            return d.depth == 1
+            return d.depth === 1
         }))
         .enter()
         .append("text")
         .attr("x", function (d) {
-            return d.x0 +30
+            return d.x0
         })
         .attr("y", function (d) {
-            return d.y0+20
+            return d.y0 + 20
         })
         .text(function (d) {
             return d.data.name
         })
-        .attr("font-size", "60px")
+        .attr("font-size", "20px")
         .attr("font-weight", "bold")
-        .attr("fill", function (d) {
-            return color(d.data.name)
-        })
+        // .attr("fill", function (d) {
+        //     return color(d.data.color)
+        // })
+        .attr("fill", "white")
 
     // Add title for the 3 groups
-    svg3
-        .append("text")
-        .attr("x", 0)
-        .attr("y", 14)    // +20 to adjust position (lower)
-        .text("Alle bedrohten Tierarten der Schweiz")
-        .attr("font-size", "60px")
-        .attr("fill", "white")
+    // svg3
+    //     .append("text")
+    //     .attr("x", 0)
+    //     .attr("y", 14)    // +20 to adjust position (lower)
+    //     .text("Alle bedrohten Tierarten der Schweiz")
+    //     .attr("text-align", "start")
+    //     .attr("font-size", "30px")
+    //     .attr("fill", "white")
 
 })
